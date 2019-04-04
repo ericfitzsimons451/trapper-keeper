920363c65a0f36d09a01e22315d63c2644be3423
Gabriel Inzurriaga <gabeinzurriaga@yahoo.com>
2019-04-04 09:21:58 -0600

    allow to delete notes from form

--- src/Containers/NoteForm/NoteForm.js
+++ src/Containers/NoteForm/NoteForm.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react'
 import { postNote } from '../../Thunks/postNote'
-
+import { Link } from "react-router-dom"
+import {ReactComponent as UnChecked } from "../../images/blank-square.svg"
+import {ReactComponent as Xmark } from "../../images/close-button.svg"
+import {ReactComponent as Checked } from "../../images/check-box.svg"
+ 
 export class NoteForm extends Component {
 	constructor() {
 		super()
 		this.state = {
 			title: '',
-			body: ''
+			body: []
 		}
+	}
+
+	addNote = () => {
+		this.setState({
+			body: [...this.state.body, {text: "hello", checked: false}]
+		})
+	}
+
+	deleteNote = (e) => {
+		const { id } = e.target.parentElement
+		let newBody = this.state.body.filter((note, index) => {
+			return index !== parseInt(id)
+		})
+		this.setState({
+			body: newBody
+		})
+	}
+
+	checkedBox = (e) => {
+		console.log(e.target.parentElement)
 	}
 
 	handleChange = (e) => {
 		const { name, value } = e.target
 		this.setState({ [name]: value })
+	}
+	
+	test = () => {
+		console.log("hello")
 	}
 
 	handleSubmit = (e) => {
@@ -22,10 +50,21 @@ export class NoteForm extends Component {
 
 	render() {
 		return (
-			<form onSubmit={this.handleSubmit}>
-				<input onChange={this.handleChange} name='title' value={this.state.title} />
-				<input onChange={this.handleChange} name='body' value={this.state.body} />
-				<button>Save Note</button>
+			<form class="note-form" onSubmit={this.handleSubmit}>
+				<input onChange={this.handleChange} name='title' value={this.state.title} placeholder="title"/>
+				{
+					this.state.body.map((text, index)=> {
+						return (<div onChange={this.test} id={index} className="text">
+									<div onClick={this.checkedBox} className="uncheckbox" id={index}></div>
+									<input  name='body' value={text.text} id={index}/>
+									<div onClick={this.deleteNote} className="xmark"></div>
+								</div>)
+					})
+				}
+				<button onClick={this.addNote}>add note</button>
+				<nav>
+					<Link to="/"><button>Save Note</button></Link>
+				</nav>
 			</form>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom'
-import "./index.css";
+import "./index.scss";
 import App from "./Containers/App/App.js";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
