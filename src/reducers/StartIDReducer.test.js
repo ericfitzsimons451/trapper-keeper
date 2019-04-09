986d111931212b5570107b1423513f16495677d0
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-04-09 10:51:54 -0600

    adds tests for startIDReducer

---
+++ src/reducers/StartIDReducer.test.js
@@ -0,0 +1,21 @@
+import * as actions from '../actions'
+import { startID } from '../reducers/startIDReducer'
+
+describe.only('startID', () => {
+    const mockState = 0
+    let mockAction;
+    it('should return state as a default', () => {
+        mockAction = {}
+        const results = startID(mockState, mockAction)
+        expect(results).toEqual(mockState)
+    })
+
+    it('should return an ID when the action.type is START_ID', () => {
+        const mockAction = {
+            type: 'START_ID',
+            id: 10
+        }
+        const results = startID(mockState, mockAction)
+        expect(results).toEqual(mockAction)
+    })
+})
