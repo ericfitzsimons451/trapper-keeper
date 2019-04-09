bd6a35792f360be8c4e763340842ae5df43b5165
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-04-09 10:42:49 -0600

    adds Create New Note button to Loader

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import './Loader.scss'
 
 export  const Loader = () => {
@@ -7,6 +8,7 @@ export  const Loader = () => {
             <h2 className="welcome">Welcome to Trapper Keeper!</h2>
             <p className="description">Trapper Keeper is a wonderful way to take notes and store ideas.</p>
             <h4 className="suggestion">Start by creating a new note now!</h4>
+            <NavLink to='/new-note'>Create New note</NavLink>
         </div>
     )
 }
